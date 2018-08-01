8df89452963a98441b30e5477c03f3c817ed314a
Samuil Gospodinov <samuil4@gmail.com>
2018-08-01 10:24:47 +0300

    adding missing props to iterface

--- src/app/shared/models/train.model.interface.ts
+++ src/app/shared/models/train.model.interface.ts
@@ -3,4 +3,6 @@ import { ITrain } from '@shared/models/train.interface';
 export interface ITrainModel extends ITrain {
   departureDate: Date;
   arrivalDate: Date;
+  name: string;
+  confirmed: boolean;
 }
